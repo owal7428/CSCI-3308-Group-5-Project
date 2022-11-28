ba4609d8877522228e76008fdcc609a5b3182f47
ethanmcaleavy <ethan.mcaleavy@gmail.com>
2022-11-27 19:04:13 -0700

    Updated SQL Table and insert function

--- All project code/init_data/create.sql
+++ All project code/init_data/create.sql
@@ -1,29 +1,23 @@
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
-    username VARCHAR(32) PRIMARY KEY, 
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(32), 
     password VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS user_trips(
     trip_id SERIAL PRIMARY Key,
-    temperature FLOAT,
-    windSpeed FLOAT,
-    flightNumber INT,
-    yearOut INT,
-    monthOut INT,
-    weekOut INT,
-    dayOut INT,
-    hourOut INT,
-    minuteOut INT,
-    yearIn INT,
-    monthIn INT,
-    weekIn INT,
-    dayIn INT,
-    hourIn INT,
-    minuteIn INT,
+    username VARCHAR(32),
+    departure DATETIME,
+    arrival DATETIME,
+    temperatureAvg FLOAT,
+    windSpeedAvg FLOAT,
+    airline VARCHAR(60),
+    airport VARCHAR(60)
     country VARCHAR(60),
     city VARCHAR(60)
 );
+
 
 CREATE TABLE user_trips_to_users(
     user_id INT,
